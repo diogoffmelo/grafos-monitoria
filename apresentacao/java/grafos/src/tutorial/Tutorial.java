13a2a683df381ff55d3b5758891174bd9ae07f2e
diogoffmelo <diogoffmelo@gmail.com>
2018-08-06 10:12:20 -0300

    Exemplos em java para aula-apresentação.

---
+++ apresentacao/java/grafos/src/tutorial/Tutorial.java
@@ -0,0 +1,99 @@
+package tutorial;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+
+import org.jgrapht.*;
+import org.jgrapht.generate.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
+import org.jgrapht.util.SupplierUtil;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+
+public class Tutorial {
+
+	public static Graph<String, DefaultEdge> createStringGraph() {
+		Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+
+		String v1 = "v1";
+		String v2 = "v2";
+		String v3 = "v3";
+		String v4 = "v4";
+
+		// add the vertices
+		g.addVertex(v1);
+		g.addVertex(v2);
+		g.addVertex(v3);
+		g.addVertex(v4);
+
+		// add edges to create a circuit
+		g.addEdge(v1, v2);
+		g.addEdge(v2, v3);
+		g.addEdge(v3, v4);
+		g.addEdge(v4, v1);
+
+		return g;
+	}
+
+
+	public static void exemploGrafoCompleto() {
+		//Copiado de https://github.com/jgrapht/jgrapht/blob/master/jgrapht-demo/src/main/java/org/jgrapht/demo/CompleteGraphDemo.java
+
+		// number of vertices
+		int SIZE = 10;
+
+		// Create the VertexFactory so the generator can create vertices
+		Supplier<String> vSupplier = new Supplier<String>()
+		{
+			private int id = 0;
+
+			@Override
+			public String get()
+			{
+				return "v" + id++;
+			}
+		};
+
+		Graph<String, DefaultEdge> completeGraph = 
+				new SimpleGraph<>(vSupplier,
+								  SupplierUtil.createDefaultEdgeSupplier(),
+								  false
+				);
+
+		// Create the CompleteGraphGenerator object
+		CompleteGraphGenerator<String, DefaultEdge> completeGenerator =
+				new CompleteGraphGenerator<>(SIZE);
+
+
+		// Use the CompleteGraphGenerator object to make completeGraph a
+		// complete graph with [size] number of vertices
+		completeGenerator.generateGraph(completeGraph);
+		//@example:generate:end
+
+		// Print out the graph to be sure it's really complete
+		Iterator<String> iter = new DepthFirstIterator<>(completeGraph);
+		while (iter.hasNext()) {
+			String vertex = iter.next();
+			System.out.println(
+					"Vertex " + vertex + " is connected to: "
+							+ completeGraph.edgesOf(vertex).toString());
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Graph<String, DefaultEdge> grafo = createStringGraph();
+		System.out.println(grafo);
+
+		System.out.println("\nExemplor de grafo completo.");
+		exemploGrafoCompleto();
+		
+
+	}
+
+}
